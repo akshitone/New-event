6895836fc126d327cd895acee8fab99f0198feb7
Viral Shastri <49094192+ViralShastri@users.noreply.github.com>
2020-10-02 11:42:43 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,7 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
